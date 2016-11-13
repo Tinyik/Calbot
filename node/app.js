52bb4b3d90423f31825d0b880e1a5f58f4507273
Tianyi Fang <fongtinyik@gmail.com>
2016-11-13 09:30:56 +0000

    Fix the fucking postback bug

--- node/app.js
+++ node/app.js
@@ -1,54 +1,56 @@
 'use strict';
 
-require('./include/global')
 
 const
-bodyParser = require('body-parser'),
-config = require('config'),
-cbmanage = require('./handlers/manage'),
-crypto = require('crypto'),
-express = require('express'),
-https = require('https'),
-mysql = require('mysql'),
-api = require('./include/api'),
-request = require('request');
+    bodyParser = require('body-parser'),
+    config = require('config'),
+    cbmanage = require('./handlers/manage'),
+    crypto = require('crypto'),
+    express = require('express'),
+    https = require('https'),
+    mysql = require('mysql'),
+    api = require('./include/api'),
+    global = require('./include/global'),
+    request = require('request');
 
 var app = express();
 app.set('port', process.env.PORT || 5000);
 app.set('view engine', 'ejs');
-app.use(bodyParser.json({ verify: verifyRequestSignature }));
+app.use(bodyParser.json({
+    verify: verifyRequestSignature
+}));
 app.use(express.static('public'));
 
 
 // Credentials
 const
-APP_SECRET        =   config.get('appSecret'),
-VALIDATION_TOKEN  =   config.get('validationToken'),
-PAGE_ACCESS_TOKEN =   config.get('pageAccessToken'),
-SERVER_URL        =   config.get('serverURL'),
-API_URL           =   config.get('apiURL'),
-DB_HOST           =   config.get('dbHost'),
-DB_USER           =   config.get('dbUser'),
-DB_PW             =   config.get('dbPassword'),
-DB_NAME           =   config.get('dbName');
+    APP_SECRET = config.get('appSecret'),
+    VALIDATION_TOKEN = config.get('validationToken'),
+    PAGE_ACCESS_TOKEN = config.get('pageAccessToken'),
+    SERVER_URL = config.get('serverURL'),
+    API_URL = config.get('apiURL'),
+    DB_HOST = config.get('dbHost'),
+    DB_USER = config.get('dbUser'),
+    DB_PW = config.get('dbPassword'),
+    DB_NAME = config.get('dbName');
 
 var connection = mysql.createConnection({
-    host        :   DB_HOST,
-    user        :   DB_USER,
-    password    :   DB_PW,
-    database    :   DB_NAME
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PW,
+    database: DB_NAME
 });
 
 
 const ContextEnum = {
-    DEFAULT : 0,
-    MANAGE  : 1,
-    INFO    : 2,
-    DUES    : 3
+    DEFAULT: 0,
+    MANAGE: 1,
+    INFO: 2,
+    DUES: 3
 }
 
 connection.connect(function(err) {
-    if (err) console.log(ErrorEnum.DBERROR);
+    if (err) console.log(global.ErrorEnum.DBERROR);
 });
 
 var context = ContextEnum.DEFAULT;
@@ -58,10 +60,11 @@ var assName = '';
 
 app.get('/webhook', function(req, res) {
     if (req.query['hub.mode'] === 'subscribe' &&
-    req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+        req.query['hub.verify_token'] === VALIDATION_TOKEN) {
         console.log("Validating webhook");
         res.status(200).send(req.query['hub.challenge']);
-    } else {
+    }
+    else {
         console.error("Failed validation. Make sure the validation tokens match.");
         res.sendStatus(403);
     }
@@ -69,13 +72,13 @@ app.get('/webhook', function(req, res) {
 
 
 /*
-* All callbacks for Messenger are POST-ed. They will be sent to the same
-* webhook. Be sure to subscribe your app to your page to receive callbacks
-* for your page.
-* https://developers.facebook.com/docs/messenger-platform/product-overview/setup#subscribe_app
-*
-*/
-app.post('/webhook', function (req, res) {
+ * All callbacks for Messenger are POST-ed. They will be sent to the same
+ * webhook. Be sure to subscribe your app to your page to receive callbacks
+ * for your page.
+ * https://developers.facebook.com/docs/messenger-platform/product-overview/setup#subscribe_app
+ *
+ */
+app.post('/webhook', function(req, res) {
     var data = req.body;
 
     // Make sure this is a page subscription
@@ -90,7 +93,8 @@ app.post('/webhook', function (req, res) {
             pageEntry.messaging.forEach(function(messagingEvent) {
                 if (messagingEvent.optin) {
                     receivedAuthentication(messagingEvent);
-                } else if (messagingEvent.message) {
+                }
+                else if (messagingEvent.message) {
                     switch (context) {
                         case ContextEnum.DEFAULT:
                             receivedMessage(messagingEvent);
@@ -101,9 +105,11 @@ app.post('/webhook', function (req, res) {
                         default:
                             break;
                     }
-                } else if (messagingEvent.delivery) {
+                }
+                else if (messagingEvent.delivery) {
                     receivedDeliveryConfirmation(messagingEvent);
-                } else if (messagingEvent.postback) {
+                }
+                else if (messagingEvent.postback) {
                     switch (context) {
                         case ContextEnum.DEFAULT:
                             receivedPostback(messagingEvent);
@@ -114,11 +120,14 @@ app.post('/webhook', function (req, res) {
                         default:
                             break;
                     }
-                } else if (messagingEvent.read) {
+                }
+                else if (messagingEvent.read) {
                     receivedMessageRead(messagingEvent);
-                } else if (messagingEvent.account_linking) {
+                }
+                else if (messagingEvent.account_linking) {
                     receivedAccountLink(messagingEvent);
-                } else {
+                }
+                else {
                     console.log("Webhook received unknown messagingEvent: ", messagingEvent);
                 }
             });
@@ -133,10 +142,10 @@ app.post('/webhook', function (req, res) {
 });
 
 /*
-* This path is used for account linking. The account linking call-to-action
-* (sendAccountLinking) is pointed to this URL.
-*
-*/
+ * This path is used for account linking. The account linking call-to-action
+ * (sendAccountLinking) is pointed to this URL.
+ *
+ */
 app.get('/authorize', function(req, res) {
     var accountLinkingToken = req.query.account_linking_token;
     var redirectURI = req.query.redirect_uri;
@@ -156,13 +165,13 @@ app.get('/authorize', function(req, res) {
 });
 
 /*
-* Verify that the callback came from Facebook. Using the App Secret from
-* the App Dashboard, we can verify the signature that is sent with each
-* callback in the x-hub-signature field, located in the header.
-*
-* https://developers.facebook.com/docs/graph-api/webhooks#setup
-*
-*/
+ * Verify that the callback came from Facebook. Using the App Secret from
+ * the App Dashboard, we can verify the signature that is sent with each
+ * callback in the x-hub-signature field, located in the header.
+ *
+ * https://developers.facebook.com/docs/graph-api/webhooks#setup
+ *
+ */
 function verifyRequestSignature(req, res, buf) {
     var signature = req.headers["x-hub-signature"];
 
@@ -170,14 +179,15 @@ function verifyRequestSignature(req, res, buf) {
         // For testing, let's log an error. In production, you should throw an
         // error.
         console.error("Couldn't validate the signature.");
-    } else {
+    }
+    else {
         var elements = signature.split('=');
         var method = elements[0];
         var signatureHash = elements[1];
 
         var expectedHash = crypto.createHmac('sha1', APP_SECRET)
-        .update(buf)
-        .digest('hex');
+            .update(buf)
+            .digest('hex');
 
         if (signatureHash != expectedHash) {
             throw new Error("Couldn't validate the request signature.");
@@ -186,13 +196,13 @@ function verifyRequestSignature(req, res, buf) {
 }
 
 /*
-* Authorization Event
-*
-* The value for 'optin.ref' is defined in the entry point. For the "Send to
-* Messenger" plugin, it is the 'data-ref' field. Read more at
-* https://developers.facebook.com/docs/messenger-platform/webhook-reference/authentication
-*
-*/
+ * Authorization Event
+ *
+ * The value for 'optin.ref' is defined in the entry point. For the "Send to
+ * Messenger" plugin, it is the 'data-ref' field. Read more at
+ * https://developers.facebook.com/docs/messenger-platform/webhook-reference/authentication
+ *
+ */
 function receivedAuthentication(event) {
     var senderID = event.sender.id;
     var recipientID = event.recipient.id;
@@ -206,8 +216,8 @@ function receivedAuthentication(event) {
     var passThroughParam = event.optin.ref;
 
     console.log("Received authentication for user %d and page %d with pass " +
-    "through param '%s' at %d", senderID, recipientID, passThroughParam,
-    timeOfAuth);
+        "through param '%s' at %d", senderID, recipientID, passThroughParam,
+        timeOfAuth);
 
     // When an authentication is received, we'll send a message back to the sender
     // to let them know it was successful.
@@ -215,19 +225,19 @@ function receivedAuthentication(event) {
 }
 
 /*
-* Message Event
-*
-* This event is called when a message is sent to your page. The 'message'
-* object format can vary depending on the kind of message that was received.
-* Read more at https://developers.facebook.com/docs/messenger-platform/webhook-reference/message-received
-*
-* For this example, we're going to echo any text that we get. If we get some
-* special keywords ('button', 'generic', 'receipt'), then we'll send back
-* examples of those bubbles to illustrate the special message bubbles we've
-* created. If we receive a message with an attachment (image, video, audio),
-* then we'll simply confirm that we've received the attachment.
-*
-*/
+ * Message Event
+ *
+ * This event is called when a message is sent to your page. The 'message'
+ * object format can vary depending on the kind of message that was received.
+ * Read more at https://developers.facebook.com/docs/messenger-platform/webhook-reference/message-received
+ *
+ * For this example, we're going to echo any text that we get. If we get some
+ * special keywords ('button', 'generic', 'receipt'), then we'll send back
+ * examples of those bubbles to illustrate the special message bubbles we've
+ * created. If we receive a message with an attachment (image, video, audio),
+ * then we'll simply confirm that we've received the attachment.
+ *
+ */
 function receivedMessage(event) {
     var senderID = event.sender.id;
     console.log(senderID);
@@ -249,10 +259,11 @@ function receivedMessage(event) {
     if (isEcho) {
         console.log(messageText);
         return;
-    } else if (quickReply) {
+    }
+    else if (quickReply) {
         var quickReplyPayload = quickReply.payload;
         console.log("Quick reply for message %s with payload %s",
-        messageId, quickReplyPayload);
+            messageId, quickReplyPayload);
 
         api.sendTextMessage(senderID, "Quick reply tapped");
         return;
@@ -263,7 +274,8 @@ function receivedMessage(event) {
         // keywords and send back the corresponding example. Otherwise, just echo
         // the text we received.
         api.sendTextMessage(senderID, messageText);
-    } else if (messageAttachments) {
+    }
+    else if (messageAttachments) {
         api.sendTextMessage(senderID, "Message with attachment received");
     }
 }
@@ -271,12 +283,12 @@ function receivedMessage(event) {
 
 
 /*
-* Delivery Confirmation Event
-*
-* This event is sent to confirm the delivery of a message. Read more about
-* these fields at https://developers.facebook.com/docs/messenger-platform/webhook-reference/message-delivered
-*
-*/
+ * Delivery Confirmation Event
+ *
+ * This event is sent to confirm the delivery of a message. Read more about
+ * these fields at https://developers.facebook.com/docs/messenger-platform/webhook-reference/message-delivered
+ *
+ */
 function receivedDeliveryConfirmation(event) {
     var senderID = event.sender.id;
     var recipientID = event.recipient.id;
@@ -288,7 +300,7 @@ function receivedDeliveryConfirmation(event) {
     if (messageIDs) {
         messageIDs.forEach(function(messageID) {
             console.log("Received delivery confirmation for message ID: %s",
-            messageID);
+                messageID);
         });
     }
 
@@ -297,12 +309,12 @@ function receivedDeliveryConfirmation(event) {
 
 
 /*
-* Postback Event
-*
-* This event is called when a postback is tapped on a Structured Message.
-* https://developers.facebook.com/docs/messenger-platform/webhook-reference/postback-received
-*
-*/
+ * Postback Event
+ *
+ * This event is called when a postback is tapped on a Structured Message.
+ * https://developers.facebook.com/docs/messenger-platform/webhook-reference/postback-received
+ *
+ */
 function receivedPostback(event) {
     var senderID = event.sender.id;
     var recipientID = event.recipient.id;
@@ -314,68 +326,47 @@ function receivedPostback(event) {
 
     // Parse payload corespondingly
     switch (payload) {
-        case 'DUES':
-        sendDuesList(senderID);
-        break;
-        case 'DROP_CLASS':
-        inputMode = 'DROP_CLASS_GET_CLASS_NAME';
-        api.sendTextMessage(senderID, 'class name?');
-        break;
-        case 'ADD_ASS':
-        inputMode = 'ADD_ASS_GET_CLASS';
-        api.sendTextMessage(senderID, 'class name?');
-        break;
         case 'MANAGE':
-        context = ContextEnum.MANAGE;
-        api.sendManageMenu(senderID);
-        break;
-        case 'ENROLL_CLASS':
-        inputMode = 'ENROLL_CLASS_GET_CLASS_NAME';
-        api.sendTextMessage(senderID, 'class name?');
-        break;
-        case 'INFO':
-        sendInfoMenu(senderID);
-        break;
-        case 'CLASS_INFO':
-        sendClassInfoMenu(senderID);
-        break;
-        case 'MY_PERSONAL_INFO':
-        sendMyPersonalInfoMenu(senderID);
-        break;
+            context = ContextEnum.MANAGE;
+            api.sendManageMenu(senderID);
+            break;
         case 'GET_STARTED':
-        connection.query('INSERT INTO users SET ?', {id: senderID},
-        function(err, result) {
-            if (err) console.log(err.code);
-        });
-        request({
-            uri: API_URL + senderID,
-            method: 'GET',
-            qs: {
-                access_token: PAGE_ACCESS_TOKEN
-            }
-        }, function(error, response, body){
-            if(error) {
-                console.log(error);
-            } else {
-                var firstName = JSON.parse(body).first_name;
-                api.sendTextMessage(senderID, 'Hi ' + firstName + ', how can I help you today?');
-            }
-        });
-        break;
+            connection.query('INSERT INTO users SET ?', {
+                    id: senderID
+                },
+                function(err, result) {
+                    if (err) console.log(err.code);
+                });
+            request({
+                uri: API_URL + senderID,
+                method: 'GET',
+                qs: {
+                    access_token: PAGE_ACCESS_TOKEN
+                }
+            }, function(error, response, body) {
+                if (error) {
+                    console.log(error);
+                }
+                else {
+                    var firstName = JSON.parse(body).first_name;
+                    api.sendTextMessage(senderID, 'Hi ' + firstName + ', how can I help you today?');
+                }
+            });
+            break;
         default:
-        // code
+            // code
     }
 
 }
 
 
 /*
-* Message Read Event
-*
-* This event is called when a previously-sent message has been read.
-* https://developers.facebook.com/docs/messenger-platform/webhook-reference/message-read
-*
-*/
+ * Message Read Event
+ *
+ * This event is called when a previously-sent message has been read.
+ * https://developers.facebook.com/docs/messenger-platform/webhook-reference/message-read
+ *
+ */
 function receivedMessageRead(event) {
     var senderID = event.sender.id;
     var recipientID = event.recipient.id;
@@ -385,17 +376,17 @@ function receivedMessageRead(event) {
     var sequenceNumber = event.read.seq;
 
     console.log("Received message read event for watermark %d and sequence " +
-    "number %d", watermark, sequenceNumber);
+        "number %d", watermark, sequenceNumber);
 }
 
 /*
-* Account Link Event
-*
-* This event is called when the Link Account or UnLink Account action has been
-* tapped.
-* https://developers.facebook.com/docs/messenger-platform/webhook-reference/account-linking
-*
-*/
+ * Account Link Event
+ *
+ * This event is called when the Link Account or UnLink Account action has been
+ * tapped.
+ * https://developers.facebook.com/docs/messenger-platform/webhook-reference/account-linking
+ *
+ */
 function receivedAccountLink(event) {
     var senderID = event.sender.id;
     var recipientID = event.recipient.id;
@@ -404,7 +395,7 @@ function receivedAccountLink(event) {
     var authCode = event.account_linking.authorization_code;
 
     console.log("Received account link event with for user %d with status %s " +
-    "and auth code %s ", senderID, status, authCode);
+        "and auth code %s ", senderID, status, authCode);
 }
 
 
--- node/handlers/enroll.js
+++ node/handlers/enroll.js
@@ -1,15 +1,14 @@
 'use strict';
 
-require('../include/global');
-
 const api = require('../include/api'),
       config = require('config'),
-      mysql = require('mysql');
+      mysql = require('mysql'),
+      global = require('../include/global');
 
 const EnrollContextEnum = {
     DEFAULT         :   0,
     GET_CLASS_NAME  :   1,
-    CONFIRM_ADD     :   2;
+    CONFIRM_ADD     :   2
 }
 
 // Credentials
@@ -34,7 +33,6 @@ var context = EnrollContextEnum.DEFAULT;
 module.exports = {
     receivedMessage : function(event) {
         var senderID = event.sender.id;
-        console.log(senderID);
         var recipientID = event.recipient.id;
         var timeOfMessage = event.timestamp;
         var message = event.message;
@@ -50,25 +48,25 @@ module.exports = {
         var messageAttachments = message.attachments;
         var quickReply = message.quick_reply;
 
-        if isEcho {
+        if (isEcho) {
             // Do nothing
-
+            return;   // FUCK YOU MUST RETURN HERE
         } else if (quickReply) {
             switch (context) {
-                case CONFIRM_ADD:
+                case EnrollContextEnum.CONFIRM_ADD:
                     if (messageText == 'Sure') {
                         context =
                         connection.query('INSERT INTO classes SET ?', {name: quickReply.payload},
                         function(err, result) {
                             if (err) {
-                                api.sendTextMessage(senderID, ErrorEnum.DBERROR);
+                                api.sendTextMessage(senderID, global.ErrorEnum.DBERROR);
                             } else {
                                 api.sendTextMessage(senderID, quickReply.payload +
                                     ' now added to database. Thanks for helping us make our service better!');
                                 connection.query('INSERT INTO user_class SET ?', {uid: senderID, cid: result.insertId},
                                 function(err, result) {
                                     if (err) {
-                                        api.sendTextMessage(senderID, ErrorEnum.DBERROR);
+                                        api.sendTextMessage(senderID, global.ErrorEnum.DBERROR);
                                     } else {
                                         api.sendTextMessage(senderID, 'Your class list now: ');
                                         api.sendDuesList(senderID);
@@ -87,21 +85,22 @@ module.exports = {
             case EnrollContextEnum.GET_CLASS_NAME:
                 connection.query('SELECT * FROM classes WHERE ?', {name: messageText},
                 function(err, rows, fields) {
-                    if (err) console.log(err.code);
+                    if (err) console.log("ERROROROROR");
                     if (rows.length != 0) {
+                        console.log("sdfsdf");
                         connection.query('INSERT INTO user_class SET ?', {uid: senderID, cid: rows[0].id})
                     } else {
+                        console.log("aaa");
                         context = EnrollContextEnum.CONFIRM_ADD;
                         var qst = 'Hmm. It seems this class is not in our database. Would you\
                         like it added to our database so that other users would be able to access it? '
                         + '(We would also add it to your class list!)';
-                        api.sendQuickReply(senderID, qst, 'Sure', messegeText, "No it's my typo :)", '');
+                        api.sendQuickReply(senderID, qst, 'Sure', messageText, "No it's my typo :)", '');
                     }
                 })
                 break;
-            default:
-
         }
+
     },
 
     receivedPostback : function(event) {
@@ -109,23 +108,22 @@ module.exports = {
         var recipientID = event.recipient.id;
         var timeOfPostback = event.timestamp;
         var payload = event.postback.payload;
+        
 
         switch (payload) {
             case 'ENROLL_CLASS':
                 context = EnrollContextEnum.GET_CLASS_NAME;
+                // api.sendTextMessage(senderID, 'Hi, which class do you want to enroll?');
+                console.log('====');
+                console.log(senderID);
+                console.log(senderID);
+                console.log('====');
                 api.sendTextMessage(senderID, 'Hi, which class do you want to enroll?');
                 break;
-            case 'DROP_CLASS':
+            //case 'DROP_CLASS':
 
-            default:
+            default:  
 
         }
     }
-
-    connection.query('INSERT INTO classes SET ?', {name: messageText},
-    function(err, result) {
-        if (err) console.log(err.code);
-        console.log(result.insertCode);
-    });
-    api.sendTextMessage(senderID, 'class = ' + messageText);
 }
